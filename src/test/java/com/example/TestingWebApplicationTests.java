ddab352f5f85837b4694e28d24dd256cf7abe96e
Thomas Roloff <phelonas@gmail.com>
2022-11-24 02:53:54 +0100

    3. adding tests

---
+++ src/test/java/com/example/TestingWebApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    private GreetingController controller;
+
+    @Test
+    public void contextLoads2() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
