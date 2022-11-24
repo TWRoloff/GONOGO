6f98fda84382307ee871d1a8cb976c40690126c8
Thomas Roloff <phelonas@gmail.com>
2022-11-24 02:27:07 +0100

    initial commit

---
+++ src/main/java/com/example/MyApplication.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RestController
+@SpringBootApplication
+
+public class MyApplication {
+
+    @Bean
+    WebMvcConfigurer configurer () {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/ui/").
+                        addResourceLocations("classpath:/static/ui/index.html");
+            }
+        };
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
